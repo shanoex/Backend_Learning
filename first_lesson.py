4553e67f42820ef183d6755a31458db1a1123d25
Tanmay <tanmay.kush11@gmail.com>
2026-06-08 12:55:12 +0530

    Intial commmit

---
+++ first_lesson.py
@@ -0,0 +1,25 @@
+#print("this is the start of the journey which will eventually help me in landing a job")
+#m=34
+#print('well') if m>34 else print('nope')
+#litaral assignment
+# first='dave'
+# last='2d'
+# print(type(first))
+# print(type(first)==str)
+# print(isinstance(first,str))
+pizza= str('pepperoni')
+print(type(pizza))
+print(isinstance(pizza,str))
+#casting a number to a string
+number=234
+dectostr=str(number)
+print(type(dectostr))
+#escaping special  characters
+sentance='I\'m a student\t Hey\nhow are you?'
+print(sentance)
+#buildin a menu
+title='menu'.upper()
+print(title.center(20, '$'))
+#some methods returning boolean data
+print('hell'.startswith('k'))
+
---
+++ rps.py
@@ -0,0 +1,27 @@
+import random
+import sys
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+print('')
+print('Welcome to the rock paper scissors game!')
+print(' Enter 1 for choosing rock,\n 2 for choosing paper, \n 3 for choosing scissors\n')
+playerschoice=int(input('Enter you choice:\n'))
+if playerschoice not in [1,2,3]:
+    print('Choose from 1,2,3 only')
+    sys.exit()
+computerchoice=random.randint(1,3)
+print("")
+print('computer choice is:', RPS(computerchoice).name)
+print('your choice is:', RPS(playerschoice).name)
+print("")
+if playerschoice == computerchoice:
+    print('It is a tie')
+elif (playerschoice == 1 and computerchoice == 3) or (playerschoice == 2 and computerchoice == 1) or (playerschoice == 3 and computerchoice == 2):
+    print('yess you win')
+else:
+    print('python wins')
