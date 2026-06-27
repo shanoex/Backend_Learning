fabbf87d36d369d3746eeba43ac6755206d744a4
Tanmay <tanmay.kush11@gmail.com>
2026-06-27 14:27:41 +0530

    completed the bank oop structure

--- OOP_project/bank.py
+++ OOP_project/bank.py
@@ -1,3 +1,7 @@
+class BalanceError(Exception):
+    pass
+
+
 class BankAccount:
     def __init__(self, account_number, account_holder, initialAmount=0):
         self.number = account_number
@@ -10,3 +14,42 @@ class BankAccount:
         self.balance += amount
         print("\nDeposit successful")
         self.get_balance()
+    def viable_Transaction(self,amount):
+        if self.balance >= amount:
+            return
+        else:
+            raise BalanceError("Insufficient funds")
+    def withdraw(self, amount):
+        try:
+            self.viable_Transaction(amount)
+            self.balance -=amount
+            print("\nWithdrawal successful")
+            self.get_balance()
+        except BalanceError as e:
+            print(f"\nWithdrawal failed: {e}")
+    def transfer(self, amount, recipient):
+        try:
+            self.viable_Transaction(amount)
+            self.withdraw(amount)
+            recipient.deposit(amount)
+            print(f"\nTransfer successful to {recipient.name}")
+        except BalanceError as e:
+            print(f"\nTransfer failed: {e}")    
+
+class InterestRewardAccount(BankAccount):
+    def deposit(self, amount):
+        self.balance = self.balance + (amount*1.05)
+        print("\nDeposit successful with interest reward")
+        self.get_balance()
+class SavingsAccount(InterestRewardAccount):
+    def __init__(self, initialAmount, account_holder):
+        super().__init__(initialAmount, account_holder)
+        self.fee = 5
+    def withdraw(self,amount):
+        try:
+            self.viable_Transaction(amount + self.fee)
+            self.balance -= (amount + self.fee)
+            print("\n Withdrawal successful")
+            self.get_balance()
+        except BalanceError as e:
+            print(f"\nWithdrawal failed: {e}")
--- OOP_project/project.py
+++ OOP_project/project.py
@@ -3,4 +3,11 @@ from bank import *
 account1= BankAccount("1000","John", 1000)
 sarah= BankAccount("1001","Sarah", 2000)
 sarah.get_balance()
-sarah.deposit(500)
+
+account1.transfer(5000, sarah)
+
+Tanmay= InterestRewardAccount("1002", "Tanmay", 500)
+Tanmay.deposit(1000)
+Tanmay.transfer(200, sarah)
+
+blaziken = SavingsAccount(1003, "Blaziken", 1000)
